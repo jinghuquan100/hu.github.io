ad6add77840e88f6881a85b90ef515797c64d0e3
jinghuquan <369484599@qq.com>
2020-01-14 19:06:40 +0800

    测试

---
+++ precache-manifest.94b3459c853093cf74346b78be8bf6bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8b2938444a96bbeae9fc",
+    "url": "/js/chunk-vendors.9d19dbd6.js"
+  },
+  {
+    "revision": "1f8eb1d05ebff18cc51f",
+    "url": "/js/app.80697fa0.js"
+  },
+  {
+    "revision": "ce3087655f094d3a47c5",
+    "url": "/js/about.1e9a17bb.js"
+  },
+  {
+    "revision": "b5f24a45dc0d24be01d061eefffc2e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f8eb1d05ebff18cc51f",
+    "url": "/css/app.42175df6.css"
+  },
+  {
+    "revision": "ce3087655f094d3a47c5",
+    "url": "/css/about.6abbe5bd.css"
+  }
+];
